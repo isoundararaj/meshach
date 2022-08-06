5ed904edddbdc3b24bf34ffbc889be73ec800f23
isoundararaj <isound@gmail.com>
2022-08-06 16:35:55 +0530

    adjustments to comparision of de-compressed file

--- a2b2a/test.sh
+++ a2b2a/test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
-
 gcc -o a2b2a *.c
 dpfx="/tmp/d"
 rm $dpfx/*.bin || echo cleaned files
 rm $dpfx/*.chk || echo cleaned files
-rm $dpfx/*.p || echo cleaned files
 f_count=0
 for in_file in $dpfx/*.txt; do
    let f_count=f_count+1
@@ -13,17 +11,20 @@ for in_file in $dpfx/*.txt; do
    bit_count="$(echo $in_file | cut -f2 -d'.')"
    ./a2b2a -a -c${bit_count} $in_file $out_file
    ./a2b2a -b -c${bit_count} $out_file $chk_file
-   tr "\n" " "< $in_file | sed 's/  */ /g' > ${in_file}.p
-   tr "\n" " "< $chk_file | sed 's/  */ /g' > ${chk_file}.p
-   diff ${in_file}.p ${chk_file}.p
+   # in the following we do:
+   # 1) replace newlines with spaces
+   # 2) ignore space at beginning of line
+   # 3) ignore space at end of line
+   # 4) multiple spaces will be replaced by a single space
+   diff <(tr '\n' ' ' < $in_file | sed 's/  */ /g;s/^ //;s/ $//')  <(tr '\n' ' ' < $chk_file | sed 's/  */ /g;s/^ //;s/ $//')
    if [ $? -ne 0 ]; then
       printf "
 
                 **** FAILED *****
 
         idx: $bit_count.  Files:
-            ${in_file}.p and
-            ${chk_file}.p
+            ${in_file} and
+            ${chk_file}
         differs
 
                 **** FAILED *****
